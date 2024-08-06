f4e0642668ceb3d76f2dd7a1fccc72fae5b1eec4
Ritik Verma <ritikverma@icloud.com>
2024-08-06 12:20:52 +0530

    implemented navbar alongside header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import Header from './components/Header/Header'
-import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
     <Header/>
-    <Navbar/>
     </>
   )
 }
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { FaSearch, FaSun, FaMoon, FaThList, FaThLarge } from 'react-icons/fa';
+import { FaSearch, FaSun, FaMoon, FaThList, FaThLarge, FaBars } from 'react-icons/fa';
 import { SiRedux } from 'react-icons/si';
 import './Header.css';
+import './Navbar.css'; // You can include this if you have specific styles for Navbar
 
 const Header = () => {
   const [isCardMode, setIsCardMode] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleModeSwitch = () => {
     setIsCardMode(!isCardMode);
@@ -16,21 +18,34 @@ const Header = () => {
     document.body.classList.toggle('dark-mode', !isDarkMode);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="header">
-      <div className="app-name">
-        <SiRedux color="green" /> <span className="app-name-text">DoIt</span>
-      </div>
-      <div className="buttons">
-        <FaSearch className="icon search-icon" />
-        <div className="icon mode-switch-icon" onClick={handleModeSwitch}>
-          {isCardMode ? <FaThList /> : <FaThLarge />}
+    <>
+      <header className="header">
+        <div className="app-name">
+          <FaBars className="icon menu-toggle-icon" onClick={toggleMenu} />
+          <SiRedux color="green" /> <span className="app-name-text">DoIt</span>
         </div>
-        <div className="icon theme-switch-icon" onClick={handleThemeSwitch}>
-          {isDarkMode ? <FaSun /> : <FaMoon />}
+        <div className="buttons">
+          <FaSearch className="icon search-icon" />
+          <div className="icon mode-switch-icon" onClick={handleModeSwitch}>
+            {isCardMode ? <FaThList /> : <FaThLarge />}
+          </div>
+          <div className="icon theme-switch-icon" onClick={handleThemeSwitch}>
+            {isDarkMode ? <FaSun /> : <FaMoon />}
+          </div>
+        </div>
+      </header>
+      <div className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+        <div className="profile-section">
+          <img src="profile_default.png" alt="Profile" className="profile-picture" />
+          <div className="greeting">Hey, ABCD</div>
         </div>
       </div>
-    </header>
+    </>
   );
 };
 
