9bdbb6483afb5bd9952235006961eb881c11be32
Ritik Verma <ritikverma@icloud.com>
2024-08-06 22:19:12 +0530

    Updated TaskList component

--- client/src/components/Hero/TaskList.jsx
+++ client/src/components/Hero/TaskList.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaStar, FaEdit } from 'react-icons/fa';
 import './TaskList.css';
 
 const TaskList = ({ tasks }) => {
+  const [starredTasks, setStarredTasks] = useState([]);
+  const [isEditMenuOpen, setIsEditMenuOpen] = useState(false);
+
+  const toggleStar = (index) => {
+    if (starredTasks.includes(index)) {
+      setStarredTasks(starredTasks.filter((i) => i !== index));
+    } else {
+      setStarredTasks([...starredTasks, index]);
+    }
+  };
+
+  const handleEditClick = () => {
+    setIsEditMenuOpen(!isEditMenuOpen);
+    // Additional logic to open the menu from the right side can be implemented here
+  };
+
   return (
     <>
       {tasks.length > 0 && (
@@ -10,6 +27,11 @@ const TaskList = ({ tasks }) => {
             <div key={index} className="task-item">
               <input type="checkbox" className="task-checkbox" />
               <div className="task-text">{task}</div>
+              <FaEdit className="task-edit" onClick={handleEditClick} />
+              <FaStar
+                className={`task-star ${starredTasks.includes(index) ? 'filled' : ''}`}
+                onClick={() => toggleStar(index)}
+              />
               <div className="task-divider"></div>
             </div>
           ))}
